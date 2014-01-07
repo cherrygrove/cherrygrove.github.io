5eb380acf1a5eb99704d43917f581b82d8a3efbc
Colin McIllece <definitestudios@gmail.com>
2014-01-07 00:09:09 -0700

    Redesigned page layout and added Google form integration

--- jquery.js
+++ jquery.js
@@ -1,12 +1,1 @@
-$(document).ready(function () {    
-    $('#Menulinks').mouseenter(
-        function () {
-            $('ul').slideDown(200);
-        },
-    });
-    $('#Menulinks').mouseleave(
-        function () {
-            $('ul').slideUp(200);            
-    });
-});
 
